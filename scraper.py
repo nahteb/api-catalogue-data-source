5af39876b3b44a7ecc62a9bb95d709de4e938471
s-cox <simon.cox@digital.cabinet-office.gov.uk>
2022-08-11 16:05:06 +0100

    Additional function to pull out API titles which is incorporated into the get_list_of_dicts to filter out urls which are API Standards

--- scraper.py
+++ scraper.py
@@ -5,21 +5,25 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 
 # function to get URLs from page
 def get_url_title(url):
-  page = requests.get(URL)
-  soup = BeautifulSoup(page.text, 'html.parser')
-  #finds all links to API/API Standards as listed on the page
-  links = [a['href'] for a in soup.find_all('a', href=True)]
-  filtereddivs = list(filter(lambda x: '/developer/api-catalogue/' in x, links))
-  beg = 'https://digital.nhs.uk'
-  urls = []
-  for link in filtereddivs:
-    # removes standards links based on 'standard' being in url
-    if 'standard' in link:
-      continue
-    else:
-      url = beg + link
-      urls.append(url)
-  return urls
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    # finds all links to API/API Standards as listed on the page
+    links = [a['href'] for a in soup.find_all('a', href=True)]
+    filtereddivs = list(filter(lambda x: '/developer/api-catalogue/' in x, links))
+    beg = 'https://digital.nhs.uk'
+    urls = []
+    for link in filtereddivs:
+        url_combined = beg + link
+        urls.append(url_combined)
+    return urls
+
+
+# function to pull out the title of the API
+def get_api_title(api_url):
+    page = requests.get(api_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    title = soup.find(class_='nhsd-t-heading-xxl nhsd-!t-margin-bottom-0').text
+    return title
 
 
 # function to pull out all information between h2 headers
@@ -51,16 +55,14 @@ def get_api_info(url):
     return headers_text_dict
 
 
-# pulls out the overview from the dictionary from the api-info function and creates a list of dictionaries
-def create_filtered_dict(apis, megadict):
+# pulls out the overview from the dictionary from the api-info function, inputs title and creates a list of dictionaries
+def create_filtered_dict(apis, megadict, title):
     new_dict = {}
     page = requests.get(apis)
     soup = BeautifulSoup(page.text, 'html.parser')
-    title = soup.find(class_='nhsd-t-heading-xxl nhsd-!t-margin-bottom-0').text
-    name = title
     description = megadict['overview']
     url = apis
-    new_dict['name'] = name
+    new_dict['name'] = title
     new_dict['description'] = description
     new_dict['url'] = url
     new_dict['contact'] = 'https://digital.nhs.uk/developer/help-and-support'
@@ -75,11 +77,14 @@ def get_list_of_dicts():
     URL = 'https://digital.nhs.uk/developer/api-catalogue'
     api_urls = get_url_title(URL)
     for apis in api_urls:
-        megadict = get_api_info(apis)
-        initial_dict = create_filtered_dict(apis, megadict)
-        filtered_list.append(initial_dict)
+        title = get_api_title(apis)
+        if 'standard' not in title:
+            megadict = get_api_info(apis)
+            initial_dict = create_filtered_dict(apis, megadict, title)
+            filtered_list.append(initial_dict)
 
     return filtered_list
+
 
 if __name__ == '__main__':
     get_list_of_dicts()
